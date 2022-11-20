5f0e83cb339f86497d68aa3575cde65c7e74dd00
Drew Hoek <drewh815@gmail.com>
2022-11-20 16:54:23 -0600

    Completed question 4

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -48,9 +48,11 @@ if (name === 'Mary') {
 // We name a variable secret but do not set it equal to any value.
 // Next we define a variable code and set it equal to the integer 123.
 // We then create a conditional that states if the variable code is exactly equal to 123 then we will execute a few things. 
-// The variable secret will now be equal to the string 'super' and the variable code will multiply by 2. code is now equal to the integer 246.
+// The variable secret will now be equal to the string 'super' and the variable code will multiply by 2. 
+// code is now equal to the integer 246.
 // We then create a second conditional that checks if the variable code is greater than 250.
-// If that is true then the variable secret is updated to the string 'duper'. This is false so the variable secret will remain equal to the string 'super'.
+// If that is true then the variable secret is updated to the string 'duper'. 
+// This is false so the variable secret will remain equal to the string 'super'.
 // We then console.log the variable secret and the string 'super' would be outputed to the console.
 
 //CODE
@@ -73,12 +75,17 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
-// We define 3 variables to start. We set the variable isStudent equal to the boolean true, the variable age equal to the integer 34 and the variable zip equal to the integer 55407.
-// We then create a conditional that first checks if the variable isStudent is true AND zip is greater than 80000. If both are true then we will output the string 'You're a student on the West Coast!' to the console. This will not output.
+// We define 3 variables to start. We set the variable isStudent equal to the boolean true, 
+// the variable age equal to the integer 34 and the variable zip equal to the integer 55407.
+// We then create a conditional that first checks if the variable isStudent is true AND zip is greater than 80000. 
+// If both are true then we will output the string 'You're a student on the West Coast!' to the console. This will not output.
 // We then get a series of else if statements that will output different strings if the previous statements are false.
-// The first else if statement checks if the variable isStudent is equal to false OR if the variable age is less than 30. If this is true then the string 'What are your hobbies?' will output to the console. This will also not output.
-// The next else if statement checks if the variable isStudent is equal to true. If this statement is true the string 'Welcome to Prime!' will output to the console. This will output to the console.
-// The last else statement will output the string 'How about the weather?' to the console if none of the above statements are true. This line will not be run as the previous else if statement was true.
+// The first else if statement checks if the variable isStudent is equal to false OR if the variable age is less than 30. 
+// If this is true then the string 'What are your hobbies?' will output to the console. This will also not output.
+// The next else if statement checks if the variable isStudent is equal to true. 
+// If this statement is true the string 'Welcome to Prime!' will output to the console. This will output to the console.
+// The last else statement will output the string 'How about the weather?' to the console if none of the above statements are true. 
+// This line will not be run as the previous else if statement was true.
 
 //CODE
 /*
@@ -135,6 +142,7 @@ if (number >= 2) {
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
 /*
+// FIX colorOne and ColorTwo are flipped, colorOne should be blue and colorTwo should be red according to the instructions.
 let colorOne = 'red';
 let colorTwo = 'blue';
 let mix = true;
